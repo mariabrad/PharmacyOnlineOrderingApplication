0c4d0e4df94cbb731f6769f48b4c4c37a2ee8fff
Ana Brad <ana.brad99@yahoo.com>
2020-06-20 18:25:59 +0300

    PHARM-42 & PHARM-60

--- src/main/java/src/main/java/CheckStock/CheckStock.java
+++ src/main/java/src/main/java/CheckStock/CheckStock.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package CheckStock;
+
+ package src.main.java.CheckStock;
 
 import ManagerPage.Manager;
 import java.sql.Connection;
