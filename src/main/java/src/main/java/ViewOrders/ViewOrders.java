725fd95f15c991bdf57ac73e2f6d0dc0427f4c06
Ana Brad <ana.brad99@yahoo.com>
2020-06-20 18:37:12 +0300

    PHARM-41 & PHARM-62

--- src/main/java/src/main/java/ViewOrders/ViewOrders.java
+++ src/main/java/src/main/java/ViewOrders/ViewOrders.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ViewOrders;
 
-import ManagerPage.Manager;
+package src.main.java.ViewOrders;
+
+import src.main.java.ManagerPage.Manager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
