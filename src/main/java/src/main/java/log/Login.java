b6ef01bd3a4a81eef065484cfb5b6d507dfeb3a9
Maria Brad <maria.brad@ymail.com>
2020-06-20 16:28:09 +0300

    PHARM-59

--- src/main/java/src/main/java/log/Login.java
+++ src/main/java/src/main/java/log/Login.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import main.java.ManagerPage.Manager;
+import src.main.java.ManagerPage.Manager;
 
 /**
  *
