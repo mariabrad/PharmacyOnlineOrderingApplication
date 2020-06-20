9d9a811cd9f783d1ff4d2d5c663ae9f848bb9ae7
Ana Brad <ana.brad99@yahoo.com>
2020-06-20 18:38:32 +0300

    PHARM-73 & PHARM-74

--- src/main/java/src/main/java/SearchItem/SearchItem.java
+++ src/main/java/src/main/java/SearchItem/SearchItem.java
@@ -1,7 +1,7 @@
 
-package SearchItem;
+package src.main.java.SearchItem;
 
-import ManagerPage.Manager;
+import src.main.java.ManagerPage.Manager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
