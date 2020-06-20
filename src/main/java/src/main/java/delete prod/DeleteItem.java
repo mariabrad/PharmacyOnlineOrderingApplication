e1ce78de02dc54fa2839b3ed6b84c5cf7e4b7abb
Ana Brad <ana.brad99@yahoo.com>
2020-06-20 18:24:02 +0300

    PHARM-9

--- src/main/java/src/main/java/delete prod/DeleteItem.java
+++ src/main/java/src/main/java/delete prod/DeleteItem.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package DeleteItem;
+package src.main.java.DeleteItem;
 
 import ManagerPage.Manager;
 import java.sql.Connection;
